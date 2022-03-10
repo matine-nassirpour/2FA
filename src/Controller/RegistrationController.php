9011a39d2bd5248637aa13e9c0ce51e4129239ec
MaddieDeveloper <matine.nassirpour@maddie.doctor>
2022-03-10 21:16:18 +0100

    feat(app): update development scripts :hammer:

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -25,7 +25,7 @@ class RegistrationController extends AbstractController
             $user->setPassword(
             $userPasswordHasher->hashPassword(
                     $user,
-                    $form->get('plainPassword')->getData()
+                    $form->get('password')->getData()
                 )
             );
 
@@ -33,7 +33,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('registration/register.html.twig', [
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,50 +5,59 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', null, [
-                'label' => 'e-mail',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez saisir un email valid',
-                    ]),
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
+                'attr' => [
+                    'autofocus' => true
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe saisis ne correspondent pas',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'label_attr' => [
+                        'title' => 'Pour des raisons de sécurité, votre mot de passe doit contenir au minimum 8 caractères dont une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial'
+                    ],
+                    'attr' => [
+                        'pattern' => '^(?=.*[a-zà-ÿ])(?=.*[A-ZÀ-Ý])(?=.*[0-9])(?=.*[^a-zà-ÿA-ZÀ-Ý0-9]).{8,}$',
+                        'title' => 'Pour des raisons de sécurité, votre mot de passe doit contenir au minimum 8 caractères dont une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial',
+                        'maxlength' => 255
+                    ]
                 ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'label_attr' => [
+                        'title' => 'Confirmer le mot de passe'
+                    ],
+                    'attr' => [
+                        'pattern' => '^(?=.*[a-zà-ÿ])(?=.*[A-ZÀ-Ý])(?=.*[0-9])(?=.*[^a-zà-ÿA-ZÀ-Ý0-9]).{8,}$',
+                        'title' => 'Confirmer le mot de passe',
+                        'maxlength' => 255
+                    ]
+                ]
             ])
             ->add('agreeTerms', CheckboxType::class, [
-                'label' => 'Accepter les conditions',
+                'label' => 'Accepter les conditions d\'utilisation',
                 'mapped' => false,
+                'required' => true,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'Veuillez accepter nos conditions',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'label' => 'Mot de passe',
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez entrer un mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'message' => 'Vous devez accepter les conditions d\'utilisation de ce site pour vous inscrire',
                     ]),
                 ],
             ])
